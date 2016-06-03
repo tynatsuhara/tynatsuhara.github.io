223ec9c05f0fd37c74d317820f576d746c1dd8c3
Tyler Bonnell <tbonnell@uw.edu>
2016-06-02 17:59:35 -0700

    cleaning stuff up (and probably breaking links)

--- parallax/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  configureParallax();
-
-//addParallax(id, img, width, height, weight, left, right, top, bottom) {
-  addParallax("mtn", "images/mtn.png", "30%", "auto", 1.5, 0, "10%", "auto", "auto", "0");
-  addParallax("mtn", "images/mtn.png", "32%", "auto", 2, 0, "0%", "auto", "auto", "0");
-  addParallax("mtn", "images/mtn.png", "32%", "auto", 3, 0, "20%", "auto", "auto", "0");
-  addParallax("mtn", "images/mtn.png", "40%", "auto", 5, 0, "0", "0", "auto", "0");
-}
