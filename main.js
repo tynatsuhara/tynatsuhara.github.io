682cc240fdd65f34e7e2d87d8129121235b4047f
Tyler Bonnell <tbonnell@uw.edu>
2016-06-14 20:11:43 -0500

    modularity

--- main.js
+++ main.js
@@ -2,34 +2,47 @@
   "use strict";
 
   window.onload = function() {
-    printName();
+    writeText("tyler bonnell", "name");
+    $("projects").onclick = function() {
+      writeText("projects", "projectsTitle")
+    }
   }
 
-  function printName() {
-    var letter = 0;
-    var name = "tyler bonnell";
-    var headerTimer = setInterval(function() {
-      console.log("shit");
-      if (letter > name.length) {
+  var cursorContainer;
+  var writeIndex;
+  var cursorInterval;
+  var writing;
+  var headerTimer;
+  function writeText(txt, id) {
+    if (cursorContainer) {
+      $(cursorContainer).innerHTML = writing;
+      if (headerTimer) clearInterval(headerTimer);
+      clearInterval(cursorInterval);
+    }
+    cursorContainer = id;
+    writing = txt;
+    writeIndex = 0;
+    headerTimer = setInterval(function() {
+      if (writeIndex > txt.length) {
         clearInterval(headerTimer);
-        setInterval(flashCursor, 500);
+        cursorInterval = setInterval(flashCursor, 500);
       } else {
-        $("#name").innerHTML = name.substring(0, letter++) + "|";
+        $(id).innerHTML = txt.substring(0, writeIndex++) + "|";
       }
     }, 90);
   }
 
   function flashCursor() {
-    var s = $("#name").innerHTML;
+    var s = $(cursorContainer).innerHTML;
     if (s[s.length - 1] == '|') {
       s = s.substring(0, s.length - 1);
     } else {
       s += "|";
     }
-    $("#name").innerHTML = s;
+    $(cursorContainer).innerHTML = s;
   }
 
   function $(el) {
-    return document.querySelector(el);
+    return document.getElementById(el);
   }
 })();
