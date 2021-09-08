00f40698feaca15e18f026eb1e73c508f6f74ac5
Tyler Bonnell <tbonnell@uw.edu>
2021-09-07 20:58:54 -0700

    nu colors

--- js.js
+++ js.js
@@ -1,24 +1,35 @@
 const KINDA_WHITE = "#f8f8f8"
 const COLORS = [
-    "#5db382",  // green
-    "#1e74d6",  // bright blue
-    "#5da3b3",  // gray blue
+    "#68a691",  // green
+    "#5c6e6d",  // sage
+    "#4f759b",  // blue
+    "#1e606e",  // green blue
     "#c75065",  // red
-    "#7d4cad",  // purp
-    "#383838",  // gray
+    "#5f2f45",  // maroon
+    "#37364e",  // midnight
 ]
-const COLOR = COLORS[Math.floor(Math.random() * (COLORS.length))]
 
-function highlight(el) {
-    el.style.color = COLOR;
-    el.style.backgroundColor = KINDA_WHITE;
+let colorIndex = Math.floor(Math.random() * (COLORS.length))
+const highlighedSide = Math.random() > 0.5 ? "a" : "b"
+
+function otherSide(id) {
+    return id === "a" ? "b" : "a"
 }
 
-function lowlight(el) {
-    el.style.color = KINDA_WHITE;
-    el.style.backgroundColor = COLOR;
+function colorize(id) {
+    const el = document.getElementById(id)
+    el.style.color = COLORS[colorIndex]
+    el.style.backgroundColor = KINDA_WHITE
+    const el2 = document.getElementById(otherSide(id))
+    el2.style.color = KINDA_WHITE
+    el2.style.backgroundColor = COLORS[colorIndex]
 }
 
-const highlighedSide = Math.random() > 0.5 ? "a" : "b";
-const a = highlight(document.getElementById(highlighedSide));
-const b = lowlight(document.getElementById(highlighedSide == "a" ? "b" : "a"));
+colorize(highlighedSide);
+
+document.onkeydown = (e) => {
+    if (e.key === "Enter") {
+        colorIndex = (colorIndex + 1) % COLORS.length
+        colorize(highlighedSide);
+    }
+}
